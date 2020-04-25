c6a2e2b21cf1c6b7ef76ff5c85f65e15ebdb8422
aravindsuram <aravindersuram@gmail.com>
2020-04-25 13:51:26 +0530

    email

--- validate_user.sh
+++ validate_user.sh
@@ -1,7 +1,9 @@
 echo "enter the firstname"
 echo "enter the lastname"
+cho "enter the emailid"
 read firstname
 read lastname
+read emailid
 user="^[A-Z]{1}[a-z]"
 ##Verify for user name
 if [[ $firstname =~ $user ]] && [[ $firstname =~ $user ]] 
@@ -9,4 +11,12 @@ then
 echo "username is validation"
 else
 echo "username is no validation"
+fi
+email="^([a-zA-Z]+[a-zA-Z0-9]*((\_|\-|\.|\+)?[[a-zA-z0-9]+))@([a-zA-Z0-9]+)+(\.([a-zA-z]{2}[a-zA-Z]*))+$"
+##Verify for emailid
+if [[ $emailid =~ $email ]]
+then
+echo "emailid correct"
+else
+echo "emailid no correct"
 fi
