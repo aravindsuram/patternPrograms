88b42cdc8c4adb77e16bb4b290a3fe97ed5400b3
aravindsuram <aravindersuram@gmail.com>
2020-04-25 13:45:49 +0530

    lastname

--- validate_user.sh
+++ validate_user.sh
@@ -1,8 +1,10 @@
 echo "enter the firstname"
+echo "enter the lastname"
 read firstname
+read lastname
 user="^[A-Z]{1}[a-z]"
 ##Verify for user name
-if [[ $firstname =~ $user ]] 
+if [[ $firstname =~ $user ]] && [[ $firstname =~ $user ]] 
 then
 echo "username is validation"
 else
